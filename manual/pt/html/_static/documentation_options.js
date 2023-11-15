b5c98b96d27dd3ecfa6b2e0a05b499ad8ac8cbd5
Tatsuhiro Tsujikawa <tatsuhiro.t@gmail.com>
2023-11-15 21:14:24 +0900

    Update doc

--- manual/pt/html/_static/documentation_options.js
+++ manual/pt/html/_static/documentation_options.js
@@ -1,12 +1,14 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.36.0',
-    LANGUAGE: 'pt_BR',
+    VERSION: '1.37.0',
+    LANGUAGE: 'pt-BR',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
     FILE_SUFFIX: '.html',
     LINK_SUFFIX: '.html',
     HAS_SOURCE: true,
     SOURCELINK_SUFFIX: '.txt',
-    NAVIGATION_WITH_KEYS: false
+    NAVIGATION_WITH_KEYS: false,
+    SHOW_SEARCH_SUMMARY: true,
+    ENABLE_SEARCH_SHORTCUTS: true,
 };
